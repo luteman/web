8e345b1383bb05cadc025c52c3618705d94139b4
kara <kara30@online.de>
2018-08-24 15:29:58 +0200

    initial commit

---
+++ wsc87959972/include/sitetree.js
@@ -0,0 +1,114 @@
+/* [nodename, id, name, navigationtext, href, isnavigation, childs[], templatename] */
+
+function jdecode(s) {
+    s = s.replace(/\+/g, "%20")
+    return unescape(s);
+}
+
+var POS_NODENAME=0;
+var POS_ID=1;
+var POS_NAME=2;
+var POS_NAVIGATIONTEXT=3;
+var POS_HREF=4;
+var POS_ISNAVIGATION=5;
+var POS_CHILDS=6;
+var POS_TEMPLATENAME=7;
+var theSitetree=[ 
+	['PAGE','4560',jdecode('Home'),jdecode(''),'/4560.html','true',[],''],
+	['PAGE','4617',jdecode('Tobias+Hecker'),jdecode(''),'/4617.html','true',[],''],
+	['PAGE','62701',jdecode('Programme'),jdecode(''),'/62701.html','true',[],''],
+	['PAGE','4671',jdecode('Gitarre'),jdecode(''),'/4671.html','true',[],''],
+	['PAGE','62401',jdecode('Laute'),jdecode(''),'/62401.html','true',[],''],
+	['PAGE','62432',jdecode('Cembalo'),jdecode(''),'/62432.html','true',[],''],
+	['PAGE','4779',jdecode('Veranstaltungen'),jdecode(''),'/4779.html','true',[],''],
+	['PAGE','20180',jdecode('Hobby%3A+Fotografie'),jdecode(''),'/20180.html','true',[],''],
+	['PAGE','4806',jdecode('Links'),jdecode(''),'/4806.html','true',[],''],
+	['PAGE','20207',jdecode('Kontakt'),jdecode(''),'/20207.html','true',[],'']];
+var siteelementCount=10;
+theSitetree.topTemplateName='Movement';
+					                                                                    
+theSitetree.getById = function(id, ar) {												
+							if (typeof(ar) == 'undefined')                              
+								ar = this;                                              
+							for (var i=0; i < ar.length; i++) {                         
+								if (ar[i][POS_ID] == id)                                
+									return ar[i];                                       
+								if (ar[i][POS_CHILDS].length > 0) {                     
+									var result=this.getById(id, ar[i][POS_CHILDS]);     
+									if (result != null)                                 
+										return result;                                  
+								}									                    
+							}                                                           
+							return null;                                                
+					  };                                                                
+					                                                                    
+theSitetree.getParentById = function(id, ar) {											
+						if (typeof(ar) == 'undefined')                              	
+							ar = this;                                             		
+						for (var i=0; i < ar.length; i++) {                        		
+							for (var j = 0; j < ar[i][POS_CHILDS].length; j++) {   		
+								if (ar[i][POS_CHILDS][j][POS_ID] == id) {          		
+									// child found                                 		
+									return ar[i];                                  		
+								}                                                  		
+								var result=this.getParentById(id, ar[i][POS_CHILDS]);   
+								if (result != null)                                 	
+									return result;                                  	
+							}                                                       	
+						}                                                           	
+						return null;                                                	
+					 }								                                    
+					                                                                    
+theSitetree.getName = function(id) {                                                    
+						var elem = this.getById(id);                                    
+						if (elem != null)                                               
+							return elem[POS_NAME];                                      
+						return null;	                                                
+					  };			                                                    
+theSitetree.getNavigationText = function(id) {                                          
+						var elem = this.getById(id);                                    
+						if (elem != null)                                               
+							return elem[POS_NAVIGATIONTEXT];                            
+						return null;	                                                
+					  };			                                                    
+					                                                                    
+theSitetree.getHREF = function(id) {                                                    
+						var elem = this.getById(id);                                    
+						if (elem != null)                                               
+							return elem[POS_HREF];                                      
+						return null;	                                                
+					  };			                                                    
+					                                                                    
+theSitetree.getIsNavigation = function(id) {                                            
+						var elem = this.getById(id);                                    
+						if (elem != null)                                               
+							return elem[POS_ISNAVIGATION];                              
+						return null;	                                                
+					  };			                                                    
+					                                                                    
+theSitetree.getTemplateName = function(id, lastTemplateName, ar) {             		 
+	                                                                                 
+	if (typeof(lastTemplateName) == 'undefined')                                     
+		lastTemplateName = this.topTemplateName;	                                 
+	if (typeof(ar) == 'undefined')                                                   
+		ar = this;                                                                   
+		                                                                             
+	for (var i=0; i < ar.length; i++) {                                              
+		var actTemplateName = ar[i][POS_TEMPLATENAME];                               
+		                                                                             
+		if (actTemplateName == '')                                                   
+			actTemplateName = lastTemplateName;		                                 
+		                                                                             
+		if (ar[i][POS_ID] == id) {                                			         
+			return actTemplateName;                                                  
+		}	                                                                         
+		                                                                             
+		if (ar[i][POS_CHILDS].length > 0) {                                          
+			var result=this.getTemplateName(id, actTemplateName, ar[i][POS_CHILDS]); 
+			if (result != null)                                                      
+				return result;                                                       
+		}									                                         
+	}                                                                                
+	return null;                                                                     
+	};                                                                               
+/* EOF */					                                                            
